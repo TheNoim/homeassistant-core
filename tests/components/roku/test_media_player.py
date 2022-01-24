482393c66b154eea8b71c95017442db9987521eb
Chris Talkington <chris@talkingtontech.com>
2022-01-23 23:29:46 -0600

    Update test_media_player.py

--- tests/components/roku/test_media_player.py
+++ tests/components/roku/test_media_player.py
@@ -75,6 +75,7 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.setup import async_setup_component
 from homeassistant.util import dt as dt_util
 
 from tests.common import MockConfigEntry, async_fire_time_changed
