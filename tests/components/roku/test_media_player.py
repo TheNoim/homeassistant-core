72b4005196b7a60713f935d7f7cdea522d9d98b6
Chris Talkington <chris@talkingtontech.com>
2022-01-23 23:21:24 -0600

    Update test_media_player.py

--- tests/components/roku/test_media_player.py
+++ tests/components/roku/test_media_player.py
@@ -629,6 +629,53 @@ async def test_media_browse(
     assert not msg["success"]
 
 
+async def test_media_browse_local(
+    hass,
+    init_integration,
+    mock_roku,
+    hass_ws_client,
+):
+    """Test browsing local media."""
+    local_media = hass.config.path("media")
+    await async_process_ha_core_config(
+        hass, {"media_dirs": {"local": local_media, "recordings": local_media}}
+    )
+    await hass.async_block_till_done()
+
+    assert await async_setup_component(hass, "media_source", {})
+    await hass.async_block_till_done()
+
+    client = await hass_ws_client(hass)
+
+    await client.send_json(
+        {
+            "id": 1,
+            "type": "media_player/browse_media",
+            "entity_id": MAIN_ENTITY_ID,
+        }
+    )
+
+    msg = await client.receive_json()
+
+    assert msg["id"] == 1
+    assert msg["type"] == TYPE_RESULT
+    assert msg["success"]
+
+    assert msg["result"]
+    assert msg["result"]["title"] == "Roku"
+    assert msg["result"]["media_class"] == MEDIA_CLASS_DIRECTORY
+    assert msg["result"]["media_content_type"] == "root"
+    assert msg["result"]["can_expand"]
+    assert not msg["result"]["can_play"]
+    assert len(msg["result"]["children"]) == 2
+
+    assert msg["result"]["children"][0]["title"] == "Apps"
+    assert msg["result"]["children"][0]["media_content_type"] == MEDIA_TYPE_APPS
+
+    assert msg["result"]["children"][1]["title"] == ""
+    assert msg["result"]["children"][1]["media_content_type"] == ""
+
+
 @pytest.mark.parametrize("mock_roku", ["roku/rokutv-7820x.json"], indirect=True)
 async def test_tv_media_browse(
     hass,
